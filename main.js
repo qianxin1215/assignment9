3f3ad6149e67c26647e7d8893a34031f7886efd0
qianxin1215 <54585318+qianxin1215@users.noreply.github.com>
2019-11-18 17:58:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const socket = new WebSocket('wss://www.el-evan.com/web_dev_chat_server'); 
+
+    socket.addEventListener('open', () => {
+        console.log('Socket, opened!'); 
+    
+        document.querySelector('#send').addEventListener('click', () => {
+            socket.send(JSON.stringify({
+                Name: document.querySelector('#name').value, 
+                Message: document.querySelector('#message').value
+            })); 
+        }); 
+    }); 
+
+    socket.addEventListener('message', socketMessage => {
+        const incoming = JSON.parse(socketMessage.data); 
+        document.querySelector('#chat-stream').innerText += '\n' + incoming.Name + '\t' + incoming.Message; 
+    }); 
+}); 
